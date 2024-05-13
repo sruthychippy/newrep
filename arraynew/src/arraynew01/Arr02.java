111b0e33aa6eb4b44bb883c15d37c767ef2b274b
SRUTHY <sruthysurendran23@gmail.com>
2024-05-13 15:22:18 +0530

    hello ..

---
+++ arraynew/src/arraynew01/Arr02.java
@@ -0,0 +1,21 @@
+package arraynew01;
+
+public class Arr02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a[]=new int[9];
+a[0]=10;
+a[1]=20;
+a[2]=30;
+a[3]=40;
+a[4]=50;
+a[5]=60;
+a[6]=70;
+a[7]=80;
+a[8]=90;
+for(int i=0;i<a.length;i++)
+	System.out.println(a[i]);
+	}
+
+}
